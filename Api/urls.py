a7f84e879f36b5261c6701ca3707aaf16dddae3b
Abdul Hanan <103137760+AbdulHanan2090@users.noreply.github.com>
2023-09-18 16:18:14 +0500

    urls.py

--- Api/urls.py
+++ Api/urls.py
@@ -2,10 +2,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import ChatCheck,Check,Filesummary
+from .views import Check,Filesummary
 urlpatterns = [
     path('', Check.as_view(), name='Checkpass'),
-    path('api/ChatCheck/<str:data>', ChatCheck.as_view(), name='Check'),
     path('api/Filesummary', Filesummary.as_view(), name='Filesummary'),
 
 ]
